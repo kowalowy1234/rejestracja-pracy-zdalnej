a374b93987b778f6f2ff3361b93d6b7621a0df7c
Piotr <kowalowy1234@gmail.com>
2022-06-08 22:37:17 +0200

    1

--- RejestracjaPracyZdalnej/RejestracjaPracyZdalnej/settings.py
+++ RejestracjaPracyZdalnej/RejestracjaPracyZdalnej/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,3 +153,5 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+django_heroku.settings(locals())
